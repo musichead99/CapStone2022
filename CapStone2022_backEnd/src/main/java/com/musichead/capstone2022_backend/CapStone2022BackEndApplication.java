f7eba323e87314d9182849868c42d2e647297d10
musichead99 <musichead99@naver.com>
2022-03-22 02:35:00 +0900

    first commit

---
+++ CapStone2022_backEnd/src/main/java/com/musichead/capstone2022_backend/CapStone2022BackEndApplication.java
@@ -0,0 +1,12 @@
+package com.musichead.capstone2022_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapStone2022BackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapStone2022BackEndApplication.class, args);
+    }
+}
